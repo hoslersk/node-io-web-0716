e9d03de4cba8d8432ab76055c575c1e4d2575957
Schyler Hosler <schylerhosler@gmail.com>
2016-11-16 16:27:10 -0500

    JS practice with Basic Node I/O

--- sleep.js
+++ sleep.js
@@ -1,5 +1,14 @@
 console.log('step 1')
+// setTimeout(function(){
+//   console.log('step 2')
+// }, 1000)
 setTimeout(function(){
-  console.log('step 2')
-}, 1000)
-console.log('step 1.5?')
+  console.log('step 4')
+}, 0) // 0 ensures second loop will address this I/O callback
+process.nextTick(function(){
+  console.log('step 3')
+}) // nextTick will occur after first loop, but BEFORE I/O callbacks
+setImmediate(function(){
+  console.log('step 5')
+}) // poorly named function for AFTER I/O callbacks
+console.log('step 2')
